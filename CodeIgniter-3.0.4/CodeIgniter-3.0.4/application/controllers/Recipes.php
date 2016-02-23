dce394c4cdbb7ee901adc15979659e7d1b8fdd8c
Natalia Ramos <nramos31@csu.fullerton.edu>
2016-02-22 17:23:54 -0800

    got json data from controll

--- CodeIgniter-3.0.4/CodeIgniter-3.0.4/application/controllers/Recipes.php
+++ CodeIgniter-3.0.4/CodeIgniter-3.0.4/application/controllers/Recipes.php
@@ -9,10 +9,28 @@ class Recipes extends CI_Controller {
 		$this->load->library("form_validation");
 	}
 	public function getRecipe()
-	{
-		
+	{	
+		$ingredients = $this->input->post();
+		$string = "";
+		for($i=0; $i<5; $i++) {
+
+			$ingredient=$ingredients['ingredient_'.$i];
+			$ingredient=trim($ingredient);
+			$ingredient=str_replace(" ", "%20", $ingredient);
+			$string .= $ingredient;
+			$string .= ",";
+
+		}
+
+		$url = "http://food2fork.com/api/search?key=0f3c05d712303cacf72b6230c8f9e793&q=" . $string;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		echo json_encode($data);
 	}
-
-
 }
 
